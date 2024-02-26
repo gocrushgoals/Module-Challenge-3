36c25c08ab8fbe71ca1c5fc9a014b4942231fe7c
gocrushgoals <gocrushgoals@gmail.com>
2024-02-26 00:33:14 -0500

    function generate password prompts

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,74 @@
-// variable arrays for password generator
+// Variable characters for password generation 
+
 var charLength = 128;
 var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specialcharArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ".", "?"];
+var specialArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ".", "?"];
 
+
+// Prompt for password characters 
 var choiceArray = [];
+// var newPassword = "null"
 
-
-// Get references to the #generate element
+// Assignment code here // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  console.log("Generate Password Button Clicked")
-
-  // 1. prompt the user for the password criteria 
-  //   a. Password length 8 < 128
-  //   b. choose to include lowercase, uppercase, numeric, and/or special characters
-    var length = prompt("Choose your password length from 8-128")
-    var lowercase = prompt("Would you like to use lowercase letters?")
-    var uppercase = prompt("would you like to use uppercase letters?")
-    var number = prompt("Would you like to include numbers?")
-
-  // 2. Selecting a prompt will be validated--validate the input 
-  // 3. At least one character type should be selected
-  // 4. Password is generated after all prompts are answered
-
-
-
-  // 5. Password is displayed in an alert or written to the page
-    return "Generated password will go here"
-}
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  var inputValues = getPrompts();
+  var passwordText = document.querySelector("#password");
+
+  if (inputValues) {
+    var newPassword = generatePassword();
+    //Stores newPassword value to be displayed 
+    passwordText.value = newPassword;
+    
+  } else {
+    passwordText.value = "Invalid Values";
+  }
+  return pwText;
+};
+
+// Generate Password based upon prompts function 
+function generatePassword() {
+  var password = "";
+  for (var i = 0; i < charLength; i++) {
+    var randomArrayIndex = Math.floor(Math.random() * choiceArray.length);
+    password = password + choiceArray[randomArrayIndex];
+  }
+  return password;
+}
+
+function getPrompts() {
+  
+  choiceArray = [];
+
+  charLength = parseInt(prompt("Input Desired Password Length between 8-128 characters"));
+
+  if (charLength < 8 || charLength > 128) {
+    alert("Password lenghth must be a number from 8 to 128. Please Try again!")
+  }
+
+  if (confirm("Include lowercase characters in your password?")) {
+    choiceArray = choiceArray.concat(lowercaseArray);
+  }
+
+  if (confirm("Include uppercase characters in your password?")) {
+    choiceArray = choiceArray.concat(uppercaseArray);
+  }
+
+  if (confirm("Include numbers in your password?")) {
+    choiceArray = choiceArray.concat(numberArray);
+  }
+
+  if (confirm("Include special characters in your password?")) {
+    choiceArray = choiceArray.concat(specialArray);
+  }
+  return true;
+}
+
